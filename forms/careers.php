f3e00005efcea9c25963cb7ea20e41962dd47bc5
Nalipi Sridhar Reddy <nalipisridharreddy@gmail.com>
2025-07-18 16:44:16 +0530

    new1

--- forms/careers.php
+++ forms/careers.php
@@ -1,57 +1,63 @@
 <?php
 
 $statusMsg = '';
+
 if (isset($_FILES["cvResume"]["name"])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $contactNum = $_POST['contactNum'];
     $jobVacancy = $_POST['jobVacancy'];
-    $jobLocation = $_POST['jobLocation'];
+    // Removed jobLocation
 
-    $fromemail =  $email;
+    $fromemail = $email;
     $subject = "Submitted CV";
-    $email_message = '<h2>CV Submitted</h2>
-                    <p><b>Name:</b> ' . $name . '</p>
-                    <p><b>Email:</b> ' . $email . '</p>
-                    <p><b>Contact #:</b> ' . $contactNum . '</p>
-                    <p><b>Job Vacancy:</b> ' . $jobVacancy . '</p>
-                    <p><b>Job Location:</b> ' . $jobLocation . '</p>';
 
-    $email_message .= "Please find the attachment";
+    // Build the email body without Job Location
+    $email_body_html = '<h2>CV Submitted</h2>
+        <p><b>Name:</b> ' . $name . '</p>
+        <p><b>Email:</b> ' . $email . '</p>
+        <p><b>Contact #:</b> ' . $contactNum . '</p>
+        <p><b>Job Vacancy:</b> ' . $jobVacancy . '</p>
+        <p>Please find the attachment below.</p>';
+
     $semi_rand = md5(uniqid(time()));
-    $headers = "From: " . $fromemail;
     $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";
 
-    $headers .= "\nMIME-Version: 1.0\n" .
-        "Content-Type: multipart/mixed;\n" .
-        " boundary=\"{$mime_boundary}\"";
+    $headers = "From: " . $fromemail . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: multipart/mixed;\r\n" .
+                " boundary=\"{$mime_boundary}\"";
+
+    $email_message = "--{$mime_boundary}\r\n";
+    $email_message .= "Content-Type: text/html; charset=\"UTF-8\"\r\n";
+    $email_message .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+    $email_message .= $email_body_html . "\r\n\r\n";
 
     if ($_FILES["cvResume"]["name"] != "") {
-        $strFilesName = $_FILES["cvResume"]["name"];
-        $strContent = chunk_split(base64_encode(file_get_contents($_FILES["cvResume"]["tmp_name"])));
+        $file_name = $_FILES["cvResume"]["name"];
+        $file_data = file_get_contents($_FILES["cvResume"]["tmp_name"]);
+        $file_content = chunk_split(base64_encode($file_data));
 
-        $email_message .= "This is a multi-part message in MIME format.\n\n" .
-            "--{$mime_boundary}\n" .
-            "Content-Type:text/html; charset=\"iso-8859-1\"\n" .
-            "Content-Transfer-Encoding: 7bit\n\n" .
-            $email_message .= "\n\n";
-
-        $email_message .= "--{$mime_boundary}\n" .
-            "Content-Type: application/octet-stream;\n" .
-            " name=\"{$strFilesName}\"\n" .
-            "Content-Transfer-Encoding: base64\n\n" .
-            $strContent  .= "\n\n" .
-            "--{$mime_boundary}--\n";
+        $email_message .= "--{$mime_boundary}\r\n";
+        $email_message .= "Content-Type: application/octet-stream;\r\n" .
+                          " name=\"{$file_name}\"\r\n";
+        $email_message .= "Content-Disposition: attachment;\r\n" .
+                          " filename=\"{$file_name}\"\r\n";
+        $email_message .= "Content-Transfer-Encoding: base64\r\n\r\n";
+        $email_message .= $file_content . "\r\n\r\n";
+        $email_message .= "--{$mime_boundary}--";
     }
 
-    $toemail = "info@craftsols.com";
+    $toemail = "khktechnologiesprivatelimited@gmail.com";
 
     if (mail($toemail, $subject, $email_message, $headers)) {
-        $statusMsg = "Email send successfully with attachment";
+        $statusMsg = "Email sent successfully with attachment.";
     } else {
-        $statusMsg = "Not sent";
+        $statusMsg = "Email not sent.";
     }
 }
+
 echo '<script type="text/javascript">
-            window.location.href = "https://craftsols.com/careers.html";
-            alert("' . $statusMsg . '"); </script>';
+    alert("' . $statusMsg . '");
+    window.location.href = "https://khktechnologies.com/careers.html";
+</script>';
